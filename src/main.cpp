33870f6e677e1a6bd6841d28ad584c1b43b25d2c
OmgRod <rlimafonseca2010@gmail.com>
2024-12-01 12:35:43 +0000

    extra debug and features

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,68 @@
 #include <Geode/modify/LevelEditorLayer.hpp>
 
 #include <alphalaneous.editortab_api/include/EditorTabs.hpp>
+#include <hjfod.gmd-api/include/GMD.hpp>
 
 using namespace geode::prelude;
 
+// Declare the global variable to store the GJGameLevel pointer
+GJGameLevel* g_currentLevel = nullptr;
+
+class $modify(MyLevelEditorLayer, LevelEditorLayer) {
+    bool init(GJGameLevel* p0, bool p1) {
+        log::debug("Initializing MyLevelEditorLayer");
+
+        if (!LevelEditorLayer::init(p0, p1)) {
+            log::debug("Failed to initialize base LevelEditorLayer");
+            return false;
+        }
+
+        // Store the GJGameLevel pointer in the global variable
+        g_currentLevel = p0;
+        log::debug("GJGameLevel pointer stored successfully");
+
+        return true;
+    }
+
+    // The geometrize method that exports the level as a GMD file
+    void geometrize(CCObject*) {
+        log::debug("Geometrize method triggered");
+
+        if (g_currentLevel) {
+            log::debug("Exporting level as GMD file");
+
+            std::filesystem::path path = Mod::get()->getTempDir() / "level.gmd";
+            log::debug("Export path: {}", path.string());
+
+            gmd::exportLevelAsGmd(g_currentLevel, path);
+            log::debug("Level export completed");
+        } else {
+            log::debug("No current level found, skipping export");
+        }
+    }
+};
+
 $execute {
-    EditorTabs::get()->registerTab(TabType::BUILD, "rawr"_spr, [](EditorUI* ui, CCMenuItemToggler* toggler) -> CCNode* { //create
+    EditorTabs::get()->registerTab(TabType::BUILD, "geometrize2gd"_spr, [](EditorUI* ui, CCMenuItemToggler* toggler) -> CCNode* {
+        log::debug("Creating geometrize button...");
 
         auto arr = CCArray::create();
-        auto label = CCLabelBMFont::create("meow", "bigFont.fnt");
-        arr->addObject(label);
+        
+        // Create the button and log it
+        auto btn = CCMenuItemSpriteExtra::create(
+            CCSprite::create("geometrizeBtn.png"_spr),
+            nullptr,
+            menu_selector(MyLevelEditorLayer::geometrize)
+        );
+        
+        // Add logging for button creation
+        if (btn) {
+            log::debug("Geometrize button created successfully");
+        } else {
+            log::debug("Failed to create geometrize button");
+        }
+        
+        arr->addObject(btn);
 
         CCLabelBMFont* textLabel = CCLabelBMFont::create("G", "bigFont.fnt");
         textLabel->setScale(0.4f);
@@ -18,24 +71,7 @@ $execute {
         EditorTabUtils::setTabIcons(toggler, textLabel, textLabel);
 
         return EditorTabUtils::createEditButtonBar(arr, ui);
-        
-    }, [](EditorUI*, bool state, CCNode*) { //toggled the tab (activates on every tab click)
-
-        log::info(":3");
-
+    }, [&](EditorUI*, bool state, CCNode*) {
+        // No additional functionality needed here
     });
 }
-
-/*
-
-class $modify(MyLevelEditorLayer, LevelEditorLayer) {
-	bool init(GJGameLevel* p0, bool p1) {
-		if (!LevelEditorLayer::init(p0, p1)) {
-			return false;
-		}
-
-		return true;
-	}
-};
-
-*/
